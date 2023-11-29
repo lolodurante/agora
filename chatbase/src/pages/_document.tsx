6c57eeeba3b95cca45de897cf67bf6cd2d42f322
Lorenzo Durante <lolodurante2003@gmail.com>
2023-11-28 22:36:54 -0300

    Initial commit

---
+++ chatbase/src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="bg-[#131316]">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
